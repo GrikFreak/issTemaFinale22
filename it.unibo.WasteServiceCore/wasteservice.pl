ee5ce2cb1f83eddc0cbee283bc0c5d3f7d255be3
GrikFreak <d.greek98@gmail.com>
2022-06-28 15:22:15 +0200

    sprint 1

--- it.unibo.WasteServiceCore/wasteservice.pl
+++
@@ -1,7 +0,0 @@
-%====================================================================================
-% wasteservice description   
-%====================================================================================
-context(ctxwasteservice, "localhost",  "TCP", "8025").
- qactor( wastetruck, ctxwasteservice, "it.unibo.wastetruck.Wastetruck").
-  qactor( wasteservice, ctxwasteservice, "it.unibo.wasteservice.Wasteservice").
-  qactor( transporttrolley, ctxwasteservice, "it.unibo.transporttrolley.Transporttrolley").
